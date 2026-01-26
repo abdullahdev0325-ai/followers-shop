cd77eee02a3531fb42626541b2f2211368a66541
ammara-Ilyas <ismarailyas545@gmail.com>
2026-01-26 19:12:16 +0500

    minor fixes in checkout form

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,30 +1,38 @@
-"use client"
-import Login from '@/components/auth/Login';
-import React from 'react';
+// "use client"
+// import Login from '@/components/auth/Login';
+// import React from 'react';
 
-// export const metadata = {
-//   title: "Aroma Flowers - Login",
-//   description:
-//     "Login to Aroma Flowers to order fresh flowers, cakes, gifts, and party decorations with fast delivery across Dubai & UAE.",
-//   keywords: [
-//     "Aroma Flowers login",
-//     "flower delivery Dubai",
-//     "UAE flower shop",
-//     "login",
-//     "gifts",
-//     "cakes",
-//     "midnight delivery",
-//   ],
-//   openGraph: {
-//     title: "Aroma Flowers - Login",
-//     description:
-//       "Login to your Aroma Flowers account and enjoy fresh flowers and gifts delivery across Dubai & UAE.",
-//     type: "website",
-//   },
+// // export const metadata = {
+// //   title: "Aroma Flowers - Login",
+// //   description:
+// //     "Login to Aroma Flowers to order fresh flowers, cakes, gifts, and party decorations with fast delivery across Dubai & UAE.",
+// //   keywords: [
+// //     "Aroma Flowers login",
+// //     "flower delivery Dubai",
+// //     "UAE flower shop",
+// //     "login",
+// //     "gifts",
+// //     "cakes",
+// //     "midnight delivery",
+// //   ],
+// //   openGraph: {
+// //     title: "Aroma Flowers - Login",
+// //     description:
+// //       "Login to your Aroma Flowers account and enjoy fresh flowers and gifts delivery across Dubai & UAE.",
+// //     type: "website",
+// //   },
+// // };
+
+// const page = () => {
+//   return <Login />;
 // };
 
-const page = () => {
-  return <Login />;
-};
+// export default page;
 
-export default page;
+import Login from '@/components/auth/Login';
+
+export const dynamic = 'force-dynamic';
+
+export default function Page() {
+  return <Login />;
+}
